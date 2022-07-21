5611302c23d11a8a24ce8207fc262fe4aab425cd
Gabriel Lucchesi <lucchesisp@gmail.com>
2022-07-21 05:22:37 -0300

    add workers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucchesisp/go-concurrency
+
+go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello Concurrency!")
+	fmt.Println("Number of CPUs:", runtime.NumCPU())
+
+	concurrent := 50
+	jobs := make(chan int, concurrent)
+	results := make(chan string)
+	files := 100
+
+	// Start the workers
+	for workerId := 0; workerId < concurrent; workerId++ {
+		go worker(workerId+1, jobs, results)
+	}
+
+	fmt.Println("Number of Goroutines:", runtime.NumGoroutine())
+
+	// Send jobs to the workers
+	for i := 0; i < files; i++ {
+		jobs <- i
+	}
+
+	// Close the jobs channel
+	close(jobs)
+
+	for i := 0; i < files; i++ {
+		fmt.Println(<-results)
+	}
+
+	close(results)
+}
+
+func worker(workerId int, jobs <-chan int, results chan<- string) {
+	for job := range jobs {
+		start := time.Now()
+		random := rand.Intn(2)
+		time.Sleep(time.Duration(random) * time.Second)
+		end := time.Now()
+
+		results <- fmt.Sprintf("THREAD_%d: Finished file%d.txt in %d milliseconds", workerId, job, end.Sub(start).Milliseconds())
+	}
 }
