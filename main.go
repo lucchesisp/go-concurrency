edc24a2d3dd0d4bae180dd0dc1a1b6c11f3fab3a
Gabriel Lucchesi <lucchesisp@gmail.com>
2022-08-01 00:43:51 -0300

    refactor number of goroutines

--- main.go
+++ main.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"runtime"
 	"time"
 )
 
+func worker(workerId int, jobs <-chan int, results chan<- string) {
+	for job := range jobs {
+		time.Sleep(1 * time.Second)
+		results <- fmt.Sprintf("THREAD_%d: Finished file%d.txt", workerId, job)
+	}
+}
+
 func main() {
-	fmt.Println("Number of CPUs:", runtime.NumCPU())
+	startProcess := time.Now()
 
-	concurrent := 50
-	jobs := make(chan int, concurrent)
+	concurrent := 1
+	files := 60
+
+	jobs := make(chan int, files)
 	results := make(chan string)
-	files := 100
 
-	// Start the workers
+	// Iniciando as goroutines
 	for workerId := 0; workerId < concurrent; workerId++ {
 		go worker(workerId+1, jobs, results)
 	}
 
-	fmt.Println("Number of Goroutines:", runtime.NumGoroutine())
-
-	// Send jobs to the workers
+	// Enviando as tarefas para as goroutines
 	for i := 0; i < files; i++ {
 		jobs <- i
 	}
 
-	// Close the jobs channel
+	// Encerrando as tarefas
 	close(jobs)
 
 	for i := 0; i < files; i++ {
@@ -35,15 +39,7 @@ func main() {
 	}
 
 	close(results)
-}
 
-func worker(workerId int, jobs <-chan int, results chan<- string) {
-	for job := range jobs {
-		start := time.Now()
-		random := rand.Intn(2)
-		time.Sleep(time.Duration(random) * time.Second)
-		end := time.Now()
-
-		results <- fmt.Sprintf("THREAD_%d: Finished file%d.txt in %d milliseconds", workerId, job, end.Sub(start).Milliseconds())
-	}
+	endProccess := time.Now()
+	fmt.Println("Total time in seconds:", endProccess.Sub(startProcess).Seconds())
 }
